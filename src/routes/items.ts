3c0edc98352fdef38e399e0997b5dd998209659d
alexindevs <alexindevs@gmail.com>
2023-09-25 01:07:54 +0100

    Fucking frustrated rn

--- src/routes/items.ts
+++ src/routes/items.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 import { createItem, getItem } from '../controllers/ItemController';
-import { upload } from '../config/multerConfig';
-import { v2 as cloudinary } from 'cloudinary';
+import { multerUploads } from '../config/multerConfig';
 import { tokenVerification } from '../controllers/userController';
 
 
@@ -12,26 +11,6 @@ import { tokenVerification } from '../controllers/userController';
  * Login/Logout and all other authorization related routes will be handled here.
  */
 
-v2.config({
-  cloud_name: 'your_cloud_name',
-  api_key: 'your_api_key',
-  api_secret: 'your_api_secret'
-});
-
-const uploadToCloudinary = async (req, res, next) => {
-  try {
-    const results = await Promise.all(
-      req.files.map(async (file) => {
-        const result = await v2.uploader.upload(file.path);
-        return result.secure_url;
-      })
-    );
-    req.cloudinaryUrls = results;
-    next();
-  } catch (error) {
-    res.status(500).json({ error: 'Error uploading to Cloudinary' });
-  }
-};
 
 const itemRouter = express.Router();
 itemRouter.use(express.json());
@@ -39,7 +18,7 @@ itemRouter.use(express.json());
 itemRouter.post(
   '/create-item',
   tokenVerification,
-  uploadToCloudinary, // Use the Cloudinary upload middleware
+  multerUploads.array('images', 5),
   createItem
 );
 itemRouter.get('/get-item', tokenVerification, getItem)
